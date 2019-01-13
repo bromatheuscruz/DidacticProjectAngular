7222097054b463ec1d85b59b872f9c2782ac16d5
Matheus Cruz <bromatheuscruz@gmail.com>
2019-01-13 03:21:38 -0200

    Ajuste para deploy

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-app.use(express.static(__dirname + '/dist/angular-deploy'))
+app.use(express.static(__dirname + '/dist/site'))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/angular-deploy/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/site/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
